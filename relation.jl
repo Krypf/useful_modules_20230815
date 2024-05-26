fdc06f1260f5a4a780efe497964e7279c89a9164
Krypf <73819781+Krypf@users.noreply.github.com>
2024-05-27 05:28:59 +0900

    relation algebra
    original: 02relation.jl

---
+++ relation.jl
@@ -0,0 +1,93 @@
+#%%
+using IterTools
+using Combinatorics
+using ProgressMeter
+
+function unit(n;start=0)#the identity element
+    return Set([(j,j) for j in start:(n+start-1)])
+end
+
+function lattice_point(n::Int64;start=0::Int64)#faster
+    X =start:(n+start-1)
+    return Set([p for p in product(X,X)])
+end
+
+function direct_prod(X,Y)
+    return Set([(x,y) for x in X, y in Y])
+end
+function composition(
+    R::Vector{Tuple{Int64, Int64}},S::Vector{Tuple{Int64, Int64}})
+    _ans = Set{Tuple{Int64, Int64}}()
+    for x in R, y in S
+        if (x[2] == y[1])
+            push!(_ans,(x[1], y[2]))
+        end
+    end
+    return _ans
+end
+#%%
+function FindInvertible(n::Int64)
+    if n ∉ 1:3
+        throw(DomainError(n, "n must be in {1, 2, 3}"))
+    end
+    X2 = lattice_point(n;start=0)
+    PX2 = powerset(collect(X2))
+    itr = 0# the number of the invertibles
+    for R in PX2, S in PX2 
+        if composition(R,S) == unit(n)
+            @show R, S
+            itr += 1
+        end
+    end
+    return itr
+end
+
+@time for n in 1:3
+    @show n
+    FindInvertible(n)
+end
+#%%
+function isreflexive(R; identity)
+    issubset(identity, Set(R))# true if reflexive
+end
+function istransitive(R)
+    issubset(composition(R,R),R)# true if transitive
+end
+#%%
+function transpose_rel(R)
+    Set([(x[2],x[1]) for x in R])
+end
+function issymmetric(R)
+    issetequal(transpose_rel(R),R)# true if symmetric
+end
+#%%
+function FindEquivalence(n)
+    X2 = lattice_point(n;start=0)
+    PX2 = powerset(collect(X2))
+    itr = 0
+    I = unit(n;start=0)
+    for R in PX2
+        if (isreflexive(R; identity=I) && istransitive(R) && issymmetric(R))
+            @show R
+            itr += 1
+        end
+    end
+    return itr
+end
+# @time FindEquivalence(4)
+
+#%%
+# 2
+
+# (R, S) = ([(0, 0), (1, 1)], [(0, 0), (1, 1)])
+# (R, S) = ([(0, 1), (1, 0)], [(0, 1), (1, 0)])
+
+# 3
+
+# (R, S) = ([(0, 0), (1, 2), (2, 1)], [(0, 0), (1, 2), (2, 1)])
+# (R, S) = ([(0, 0), (1, 1), (2, 2)], [(0, 0), (1, 1), (2, 2)])
+# (R, S) = ([(1, 2), (2, 0), (0, 1)], [(0, 2), (2, 1), (1, 0)])
+# (R, S) = ([(2, 0), (1, 1), (0, 2)], [(2, 0), (1, 1), (0, 2)])
+# (R, S) = ([(0, 1), (2, 2), (1, 0)], [(0, 1), (2, 2), (1, 0)])
+# (R, S) = ([(0, 2), (2, 1), (1, 0)], [(1, 2), (2, 0), (0, 1)])
+#   0.250575 seconds (2.89 M allocations: 342.603 MiB, 20.54% gc time)
